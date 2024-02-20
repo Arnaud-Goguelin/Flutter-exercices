b135e0458bb46f0502445aa1059262ec53cfa7e8
Arnaud Goguelin <arnaud.goguelin@gmail.com>
2024-02-20 15:41:03 +0100

    création entête

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -31,6 +31,41 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.onPrimary,
+          toolbarHeight: 120,
+          centerTitle: true,
+          title: const Column(
+            children: [
+              Text(
+                HomePage.title,
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 32.0,
+                ),
+              ),
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  'What\'s need to be done?',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+              Divider(
+                color: Colors.grey,
+                height: 50,
+                thickness: 2,
+              )
+            ],
+          ),
+        ),
+        body: ListView(
+          children: [Text('')],
+        ));
   }
 }
